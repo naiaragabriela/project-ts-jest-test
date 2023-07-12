0372ef478b67596044e4d4bb87cb24ebeee94b30
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-12 11:24:57 -0300

    test: create examples test jest assertions

---
+++ src/exemples/test-it-describe.spec.ts
@@ -0,0 +1,22 @@
+describe('Testando alguma coisa', () => {
+  it('descrição do teste (IT)', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('Testando alguma coisa', () => {
+  test('descrição do teste (TESTE)', () => {
+    const name = 'Naiara';
+    expect(name).toBe('Naiara');
+  });
+});
+
+it('descrição do teste (IT)', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+test('descrição do teste (TESTE)', () => {
+  const name = 'Naiara';
+  expect(name).toBe('Naiara');
+});
